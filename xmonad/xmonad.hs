4ae07582c2f62571bfb8f8410369474e169ffbb2
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-02-16 15:03:58 -0200

    Add some visual basic stuff

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,11 +2,20 @@ import XMonad
 import XMonad.Config.Desktop
 
 main = xmonad desktopConfig
-  { terminal    = myTerminal
-  , modMask     = myModMask
-  , borderWidth = myBorderWidth
+  { terminal              = myTerminal
+  , modMask               = myModMask
+  , borderWidth           = myBorderWidth
+  , focusFollowsMouse     = myFocusFollowsMouse
+  , clickJustFocuses      = myClickJustFocuses
+  , normalBorderColor     = myNormalBorderColor
+  , focusedBorderColor    = myFocusedBorderColor
   }
 
 myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
 myBorderWidth = 1
+
+myNormalBorderColor   = "black"
+myFocusedBorderColor  = "#4693D8"
+myFocusFollowsMouse   = False
+myClickJustFocuses    = False
