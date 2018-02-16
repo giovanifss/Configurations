dd2a37911b3aafa654b08695b0b7ccebf3b259c4
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-02-16 16:51:17 -0200

    Add xmobar to xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,15 +1,27 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
 
-main = xmonad desktopConfig
-  { terminal              = myTerminal
-  , modMask               = myModMask
-  , borderWidth           = myBorderWidth
-  , focusFollowsMouse     = myFocusFollowsMouse
-  , clickJustFocuses      = myClickJustFocuses
-  , normalBorderColor     = myNormalBorderColor
-  , focusedBorderColor    = myFocusedBorderColor
-  }
+main = do
+  xmproc <- spawnPipe "/path/to/xmobar .xmobarrc"
+  xmonad $ desktopConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , terminal              = myTerminal
+    , logHook = dynamicLogWithPP xmobarPP
+        { ppOutput = hPutStrLn xmproc
+        , ppTitle = xmobarColor "green" "" . shorten 50
+        }
+    , modMask               = myModMask
+    , borderWidth           = myBorderWidth
+    , focusFollowsMouse     = myFocusFollowsMouse
+    , clickJustFocuses      = myClickJustFocuses
+    , normalBorderColor     = myNormalBorderColor
+    , focusedBorderColor    = myFocusedBorderColor
+    }
 
 myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
