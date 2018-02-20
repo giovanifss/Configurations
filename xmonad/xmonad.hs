5addae670cfc596e5b9681ae93c845a584bc71c7
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-02-20 17:37:27 -0300

    Add xmobar to all displays

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,20 +1,26 @@
 import XMonad
 import XMonad.Config.Desktop
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.Hooks.DynamicBars as Bars
+import qualified XMonad.Actions.CopyWindow as CopyW
+import qualified XMonad.Hooks.DynamicLog as DLog
+import qualified XMonad.Hooks.WorkspaceHistory as WH
+
 import System.IO
 
 main = do
-  xmproc <- spawnPipe "<XMOBAR-BIN> <XMOBAR-RC>"
-  xmonad $ desktopConfig
+  xmonad $ docks $ desktopConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , startupHook = do
+        Bars.dynStatusBarStartup xmobarCreator xmobarDestroyer
+    , logHook = do
+        copies <- CopyW.wsContainingCopies
+        WH.workspaceHistoryHook
+        Bars.multiPP (myLogPPActive copies) (myLogPP copies)
+    , handleEventHook = Bars.dynStatusBarEventHook xmobarCreator xmobarDestroyer
     , terminal              = myTerminal
-    , logHook = dynamicLogWithPP xmobarPP
-        { ppOutput = hPutStrLn xmproc
-        , ppTitle = xmobarColor "green" "" . shorten 50
-        }
     , modMask               = myModMask
     , borderWidth           = myBorderWidth
     , focusFollowsMouse     = myFocusFollowsMouse
@@ -31,3 +37,21 @@ myNormalBorderColor   = "black"
 myFocusedBorderColor  = "#404040"
 myFocusFollowsMouse   = False
 myClickJustFocuses    = False
+
+-- Xmobar multiple screens --
+xmobarCreator :: Bars.DynamicStatusBar
+xmobarCreator (S sid) = spawnPipe $ "<XMOBAR-BIN> <XMOBAR-RC> --screen " ++ show sid
+
+xmobarDestroyer :: Bars.DynamicStatusBarCleanup
+xmobarDestroyer = return ()
+
+myLogPP :: [WorkspaceId] -> DLog.PP
+myLogPP copies = DLog.defaultPP
+
+myLogPPActive :: [WorkspaceId] -> DLog.PP
+myLogPPActive copies = (myLogPP copies)
+  { DLog.ppCurrent = DLog.xmobarColor myCurrentBG myNormalFG . DLog.pad
+  }
+
+myCurrentBG = "#888888"
+myNormalFG = "#ffffff"
