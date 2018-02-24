bd1d091893d16a1dcfbd6b8da918bd2265aeb249
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-02-23 22:14:18 -0300

    Add custom keybindings

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,6 +2,9 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Actions.GridSelect (goToSelected, defaultGSConfig)
+import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 import qualified XMonad.Hooks.DynamicBars as Bars
 
 import System.IO
@@ -20,6 +23,7 @@ main = do
     , clickJustFocuses      = myClickJustFocuses
     , normalBorderColor     = myNormalBorderColor
     , focusedBorderColor    = myFocusedBorderColor
+    , keys                  = myKeys
     }
 
 myTerminal    = "terminator"
@@ -37,3 +41,22 @@ xmobarCreator (S sid) = spawnPipe $ "<XMOBAR-BIN> <XMOBAR-RC> --screen " ++ show
 
 xmobarDestroyer :: Bars.DynamicStatusBarCleanup
 xmobarDestroyer = return ()
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+  [ ((modm, xK_Return), spawn $ XMonad.terminal conf)                           -- Launch terminal
+  , ((modm, xK_p), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")      -- Launch dmenu
+  , ((modm, xK_c), kill)                                                        -- Close focused window
+  , ((modm, xK_k), windows W.focusUp)                                           -- Move focus to the previous window
+  , ((modm, xK_j), windows W.focusDown)                                         -- Move focus to the next window
+  , ((modm, xK_t), withFocused $ windows . W.sink)                              -- Push window back into tiling
+  ]
+  ++
+  [((m .|. modm, k), windows $ f i)                                             -- mod-shift-[1..9], Move client to workspace N
+    | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]                     -- mod-[1..9], Switch to workspace N
+  , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+  ++
+  [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))      -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+    | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]                                 -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+  , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+  ++
+  [((modm, xK_g), goToSelected defaultGSConfig)]                                -- GridSelect
