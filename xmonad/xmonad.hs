27360e3f3ec6563c7912896de7509d717376246d
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-03-18 14:01:31 -0300

    Add automatic spawn of monitoring panel

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -20,7 +20,8 @@ main = do
     , layoutHook = avoidStruts  $  layoutHook def
     , startupHook = do
         Bars.dynStatusBarStartup xmobarCreator xmobarDestroyer
-        spawn "xmodmap -e 'keysym Menu = Super_R'"    -- Make xk_menu key be xk_super
+        spawn "xmodmap -e 'keysym Menu = Super_R'"        -- Make xk_menu key be xk_super
+        spawnOnWorkspace "1_9" "terminator --layout=monitoring"    -- Spawn monitoring tools on workspace 9
     , handleEventHook = Bars.dynStatusBarEventHook xmobarCreator xmobarDestroyer
     , logHook = Bars.multiPP myLogPPActive myLogPP
     , terminal              = myTerminal
@@ -33,6 +34,10 @@ main = do
     , workspaces            = IS.withScreens nScreens (map show [1..9])
     , keys                  = myKeys
     }
+
+spawnOnWorkspace :: String -> String -> X ()
+spawnOnWorkspace ws program = do spawn program
+                                 windows $ W.greedyView ws
 
 -- Usability --
 myTerminal    = "terminator"
