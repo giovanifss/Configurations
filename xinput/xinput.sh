74b54198f0e0b70782a98bf89ee13e2ed315c0c5
giovanifss <11573379+giovanifss@users.noreply.github.com>
2018-05-28 17:45:00 -0300

    Add better option to enable natural scrolling

--- xinput/xinput.sh
+++ xinput/xinput.sh
@@ -1,1 +1,2 @@
 xinput set-button-map 11 1 2 3 5 4 7 6    # Natural scrolling up <-> down and left <-> right
+xinput --set-prop 12 288 1                # Natural scrolling up <-> down and left <-> right
